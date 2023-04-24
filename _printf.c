6ef9a94116a152682e5a4a276d7ceeb1ffbaa810
Mucyo1 <mucyoprinceherve@example.com>
2023-04-24 06:07:45 -0700

    second task

--- _printf.c
+++ _printf.c
@@ -9,8 +9,12 @@ void print_buffer(char buffer[], int *buff_ind);
  */
 int _printf(const char *format, ...)
 {
-	int i, printed = 0, printed_chars = 0;
-	int flags, width, precision, size, buff_ind = 0;
+	int x,  printed = 0;
+	int printed_chars = 0;
+	int flags;
+	int width;
+	int precision;
+	int size, buff_ind = 0;
 	va_list list;
 	char buffer[BUFF_SIZE];
 
@@ -19,25 +23,25 @@ int _printf(const char *format, ...)
 
 	va_start(list, format);
 
-	for (i = 0; format && format[i] != '\0'; i++)
+	for (x = 0; format && format[x] != '\0'; x++)
 	{
-		if (format[i] != '%')
+		if (format[x] != '%')
 		{
-			buffer[buff_ind++] = format[i];
+			buffer[buff_ind++] = format[x];
 			if (buff_ind == BUFF_SIZE)
 				print_buffer(buffer, &buff_ind);
-			/* write(1, &format[i], 1);*/
+			/* write(1, &format[x], 1);*/
 			printed_chars++;
 		}
 		else
 		{
 			print_buffer(buffer, &buff_ind);
-			flags = get_flags(format, &i);
-			width = get_width(format, &i, list);
-			precision = get_precision(format, &i, list);
-			size = get_size(format, &i);
-			++i;
-			printed = handle_print(format, &i, list, buffer,
+			flags = get_flags(format, &x);
+			width = get_width(format, &x, list);
+			precision = get_precision(format, &x, list);
+			size = get_size(format, &x);
+			++x;
+			printed = handle_print(format, &x, list, buffer,
 				flags, width, precision, size);
 			if (printed == -1)
 				return (-1);
